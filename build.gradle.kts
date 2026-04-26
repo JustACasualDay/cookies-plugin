4a542a5f0a6bc3c96c6ad5a76125e4658c35d3af
JustACasualDay <samuel.kaltenegger@gmail.com>
2026-04-26 08:20:32 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("java-library")
+    id("com.gradleup.shadow") version "9.4.1"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+val minecraftVersion: String by project
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://nexus.justacasualday.com/repository/maven-snapshots")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot:$minecraftVersion-R0.1-SNAPSHOT")
+    implementation("at.justacasualday:JustACasualAPI:$minecraftVersion-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    runServer {
+        minecraftVersion(minecraftVersion)
+    }
+}
---
+++ src/main/java/at/justacasualday/cookies/Cookies.java
@@ -0,0 +1,16 @@
+package at.justacasualday.cookies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cookies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
